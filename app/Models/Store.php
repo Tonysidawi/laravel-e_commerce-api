bedf74c6ef487d5c5c95b1befff591bb315160f4
Tonysidawi <anthonysidawi1990@gmail.com>
2026-06-22 11:57:34 +0000

    added has image trait to prevent code duplication

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\HasImage;
+use App\Traits\HasImage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
